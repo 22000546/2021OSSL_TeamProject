7c1b318fdbf885d5ef7b68858b20c8f125f42eb6
Yeeun <22000546@handong.edu>
2021-05-01 17:33:02 +0900

    delete manager.h

--- TeamProject/manager.h
+++
@@ -1,6 +0,0 @@
-int searchName(course *c, int index);
-int searchCredit(course *c, int index);
-int searchGrade(course *c, int index);
-float calculateTotalGrade(course *c, int index);
-float calculateMajorGrade(course *c, int index);
-float calculateLAGrade(course *c, int index);
