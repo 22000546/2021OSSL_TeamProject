0ec119e641914c7cea42700fdae3791ada27ad71
Yeeun <22000546@handong.edu>
2021-05-04 11:23:07 +0900

    linked lists 방식으로 구현

--- inc/manager.h
+++ inc/manager.h
@@ -3,20 +3,20 @@
 #include "essential.h"
 
 // 과목 이름으로 검색하는 함수
-int searchName(course *c, int index); 
+int searchName(Node* course); 
 
 // 과목의 학점으로 검색하는 함수
-int searchCredit(course *c, int index); 
+int searchCredit(Node* course); 
 
 // 평점으로 검색하는 함수
-int searchGrade(course *c, int index); 
+int searchGrade(Node* course); 
 
 // 모든 입력된 데이터의 평점 평균을 계산하는 함수
-float calculateTotalGrade(course *c, int index); 
+float calculateTotalGrade(Node* course); 
 
 // 입력된 데이터 중 전공 과목의 평점 평균을 계산하는 함수
-float calculateMajorGrade(course *c, int index);
+float calculateMajorGrade(Node* course);
 
 // 입력된 데이터 중 교양 과목의 평점 평균을 계산하는 함수
-float calculateElectiveGrade(course *c, int index);
+float calculateElectiveGrade(Node* course);
 #endif
