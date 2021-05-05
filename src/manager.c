d5d139db72f8042a20bd9562f7b76d24aebb9b1b
Yeeun <22000546@handong.edu>
2021-05-05 21:43:51 +0900

    searchGrade 수정

--- src/manager.c
+++ src/manager.c
@@ -68,8 +68,11 @@ void searchGrade(Node* course) {
   printf("찾으시는 과목의 평점을 입력해주세요 : ");
   scanf("%s", search);
 
+  // 평점이 A+, A0, B+, B0, C+, C0, D+, D0, F 로 처리되었다고 가정
   for(Node* i = course; i != NULL; i = i->next) {
-    if(i->grade == search) {
+    if((i->grade[0] == search[0])&&(i->grade[1] == search[1])) {
+      scnt ++;
+    } else if((search[0] = 'F')&&(i->grade[0] = 'F')) {
       scnt ++;
     }
   }
@@ -80,7 +83,7 @@ void searchGrade(Node* course) {
     printf("===========================================\n");
     int index = 1;
     for(Node* i = course; i != NULL; i = i->next) {
-     if(i->grade == search) {
+     if(((i->grade[0] == search[0])&&(i->grade[1] == search[1]))||((search[0] = 'F')&&(i->grade[0] = 'F'))) {
         printf("%02d     %s       %-8s       %d       %s\n", index, courseType(i->type), i->name, i->credit, i->grade);
         index ++;
      }
